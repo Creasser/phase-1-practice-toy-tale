57f6a04235e28f020f9c539656f09cd1f0333a52
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-22 21:19:16 -0600

    commit tree 033b229958cea4283d04171edb27a60e7bea42cb

--- src/index.js
+++ src/index.js
@@ -13,3 +13,77 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//Adding all current toys in the db.json to the DOM. Starts here
+function fetchAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toyData => initialize(toyData))
+}
+
+const toyContainer = document.getElementById('toy-collection')
+
+function addOneToyCard(toy){
+  let card = document.createElement('div')
+  card.className = 'card'
+  card.innerHTML = `
+  <h2>${toy.name}</h2>
+  <img src ='${toy.image}' class='toy-avatar' />
+  <p>${toy.likes}</p>
+  <button class='like-btn' id='${toy.id}'>Like ❤️</button>
+  `
+  toyContainer.appendChild(card)
+  let likeBtn = document.getElementById(`${toy.id}`)
+  likeBtn.addEventListener('click', () => {
+    toy.likes += 1
+    card.querySelector('p').textContent = toy.likes
+    sendLikesToServer(toy)
+  })
+}
+
+function initialize(toyData){
+  toyData.forEach(toy => addOneToyCard(toy))
+}
+fetchAllToys()
+//END
+//Adding a new toy using POST starts here.
+
+let form = document.querySelector('form')
+let toyForm = document.querySelector('.container')
+form.addEventListener('submit', createNewToy)
+
+function createNewToy(e){
+  e.preventDefault()
+  let toyObj = {
+    'name': e.target.name.value,
+    'image': e.target.image.value,
+    'likes': 0,
+  }
+  submitNewToy(toyObj)
+}
+
+function submitNewToy(toyObj){
+  console.log(toyObj)
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body:JSON.stringify(toyObj)
+  })
+  .then(resp => resp.json())
+  .then(toy => addOneToyCard(toy))
+}
+
+function sendLikesToServer(toy){
+  fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
